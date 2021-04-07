770afcb7a2a5b8ae8f0a30e7291036dc75c27909
Ajinkya Dhote <ajinkya65@gmail.com>
2021-04-06 22:44:26 -0500

    Adding Color Attribute
    *The first triangle should show Fragment Interpolation.

--- LearnOpenGL/Source/main.cpp
+++ LearnOpenGL/Source/main.cpp
@@ -6,23 +6,24 @@
 //VertexShader
 const char* vertexShaderSource = "#version 330 core\n"
 "layout (location=0) in vec3 aPos;\n"
+"layout (location=1) in vec3 aColor;\n"
 
-"out vec4 vertexColor;\n"
+"out vec3 vertexColor;\n"
 
 "void main()\n"
 "{\n"
 "gl_Position = vec4(aPos, 1.0); \n"
-"vertexColor = vec4(0.5, 0.0, 0.0, 1.0);\n"
+"vertexColor = aColor;\n"
 "}\0";
 
 
 //FragmentShader
 const char* fragmentShaderSource = "#version 330 core\n"
-"uniform vec4 ourColor;\n"
+"in vec3 vertexColor;\n"
 "out vec4 FragColor;\n"
 "void main()\n"
 "{\n"
-"FragColor = ourColor;\n"
+"FragColor = vec4(vertexColor, 1.0);\n"
 "}\0";
 
 
@@ -150,9 +151,9 @@ int main()
 
     //Set up vertex Data (and buffer[s]) and configure vertex attribs
     float vertices[] = {
-    0.0f, 0.0f, 0.0f, //Common Origin 0
-    -1.0f, 0.0f, 0.0f, //Left Vertex  2
-    -0.5f, 0.5f, 0.0f, //Top Left     3
+    0.0f, 0.0f, 0.0f, /*Common Origin 0*/ 1.0f, 0.0f, 0.0f, /*Red Color*/
+    -1.0f, 0.0f, 0.0f, /*Left Vertex  2*/ 0.0f, 1.0f, 0.0f, /*Green Color*/
+    -0.5f, 0.5f, 0.0f, /*Top Left     3*/ 0.0f, 0.0f, 1.0f, /*Blue Color*/
     };
 
     float vertices1[] = {
@@ -177,8 +178,12 @@ int main()
     glBindVertexArray( VAOs[0] );
     glBindBuffer( GL_ARRAY_BUFFER, VBOs[0] );
     glBufferData( GL_ARRAY_BUFFER, sizeof( vertices ), vertices, GL_STATIC_DRAW );
-    glVertexAttribPointer( 0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof( float ), (void*)0 );
+    glVertexAttribPointer( 0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof( float ), ( void* )0 );
     glEnableVertexAttribArray( 0 );
+
+    glVertexAttribPointer( 1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof( float ), ( void* )( 3 * sizeof( float ) ) );
+    glEnableVertexAttribArray( 1 );
+
 
     //Setup 2nd Triangle
     glBindVertexArray( VAOs[1] );
@@ -229,17 +234,17 @@ int main()
         processInput(window);
 
         //rendering commands...
-        glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
         //Activate the shader
         glUseProgram( shaderProgram );
 
         //draw our first triangle
-        float timeValue = glfwGetTime();
-        float greenValue = ( sin( timeValue ) / 2.0f ) + 0.5f;
-        int vertexColorLocation = glGetUniformLocation( shaderProgram, "ourColor" );
-        glUniform4f( vertexColorLocation, 0.0f, greenValue, 0.0f, 0.0f );
+        //float timeValue = glfwGetTime();
+        //float greenValue = ( sin( timeValue ) / 2.0f ) + 0.5f;
+        //int vertexColorLocation = glGetUniformLocation( shaderProgram, "ourColor" );
+        //glUniform4f( vertexColorLocation, 0.0f, greenValue, 0.0f, 0.0f );
 
         glBindVertexArray( VAOs[0] ); //seeing as we only have a single VAO there's no need to bind it every time(reason for binding it the second time, but we'll do so to keep things a bit more organized
         glDrawArrays( GL_TRIANGLES, 0, 3 );
